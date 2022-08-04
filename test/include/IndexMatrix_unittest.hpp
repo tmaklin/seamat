2daa662b7f2ee414a5f57c24522220dc8bd8d337
Tommi Mäklin <tommi@maklin.fi>
2022-08-04 16:22:16 +0300

    Add missing license boilerplates.

--- test/include/IndexMatrix_unittest.hpp
+++ test/include/IndexMatrix_unittest.hpp
@@ -1,3 +1,12 @@
+// seamat: templatized matrix library
+// https://github.com/tmaklin/seamat
+//
+// Copyright (C) 2021 Tommi Mäklin (tommi@maklin.fi)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
 #ifndef SEAMAT_INDEXMATRIX_UNITTEST_HPP
 #define SEAMAT_INDEXMATRIX_UNITTEST_HPP
 
